71f5e30797d83d0158da4645211b5f54178929c1
CasianLW <ciorbcasian@gmail.com>
2023-12-05 12:35:16 +0100

    utils/bodyParser for chunks

---
+++ utils/bodyParser.js
@@ -0,0 +1,24 @@
+async function bodyParser(request) {
+  return new Promise((resolve, reject) => {
+    let totalData = "";
+
+    request.on("data", (chunk) => {
+      totalData += chunk.toString();
+    });
+
+    request.on("end", () => {
+      try {
+        request.body = JSON.parse(totalData);
+        resolve();
+      } catch (error) {
+        reject(error);
+      }
+    });
+
+    request.on("error", (error) => {
+      reject(error);
+    });
+  });
+}
+
+module.exports = bodyParser;
