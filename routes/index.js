a302d07f675974d74c08fcf3df1315e68f27ab94
CasianLW <ciorbcasian@gmail.com>
2023-12-18 16:51:43 +0100

    Routes inside routesSetup (for user crud)

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,22 @@
 import UserController from "../app/Controllers/UserController.js";
 
-const setupRoutes = (app) => {
-  app.on("/users", (req, res) => {
-    const userController = new UserController(req, res);
-    userController.getUsers(); // Assuming getUsers is a method in UserController
-  });
+const setupRoutes = (req, res) => {
+  const userController = new UserController(req, res);
 
-  // Add other routes here
+  if (req.url === "/users" && req.method === "GET") {
+    userController.getUsers();
+  } else if (req.url.match(/\/users\/\w+/) && req.method === "GET") {
+    userController.getUser();
+  } else if (req.url === "/users" && req.method === "POST") {
+    userController.createUser();
+  } else if (req.url.match(/\/users\/\w+/) && req.method === "PUT") {
+    userController.updateUser();
+  } else if (req.url.match(/\/users\/\w+/) && req.method === "DELETE") {
+    userController.deleteUser();
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Route not found" }));
+  }
 };
 
 export default setupRoutes;
